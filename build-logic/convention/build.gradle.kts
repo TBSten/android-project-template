3f49bb2cdce754f5f4464ffdac4a346fb8567000
TBSten <81161390+TBSten@users.noreply.github.com>
2024-12-09 00:21:32 +0900

    build-logic と lint plugin を追加

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,34 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    `kotlin-dsl`
+}
+
+group = "build.logic"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
+}
+
+dependencies {
+    implementation(libs.kotlinGradlePlugin)
+    implementation(libs.kotlinGradlePluginApi)
+
+    implementation(libs.ktlintGradlePlugin)
+}
+
+gradlePlugin {
+    plugins {
+        register("lint") {
+            id = "buildLogic.lint"
+            implementationClass = "LintPlugin"
+        }
+    }
+}
---
+++ build-logic/convention/src/main/kotlin/LintPlugin.kt
@@ -0,0 +1,26 @@
+import dsl.alias
+import dsl.ktlint
+import dsl.libs
+import dsl.plugin
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.jlleitschuh.gradle.ktlint.tasks.GenerateReportsTask
+
+open class LintPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            pluginManager.apply {
+                alias(libs.plugin("ktlint"))
+            }
+
+            ktlint {
+            }
+
+            tasks.withType(GenerateReportsTask::class.java) {
+                reportsOutputDirectory.set(
+                    rootProject.layout.buildDirectory.dir("ktlint-report"),
+                )
+            }
+        }
+    }
+}
