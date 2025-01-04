4cca34220d5ac37fe1edfd9b4e5f5015f6dd3e1e
TBSten <81161390+TBSten@users.noreply.github.com>
2025-01-04 18:56:58 +0900

    android lint 向け不正なコード

--- feature/example/src/main/kotlin/your/projectPackage/feature/example/Navigation.kt
+++ feature/example/src/main/kotlin/your/projectPackage/feature/example/Navigation.kt
@@ -1,11 +1,11 @@
 package your.projectPackage.feature.example
 
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
 import kotlinx.serialization.Serializable
 import your.projectPackage.feature.example.top.ExampleTopScreen
 import your.projectPackage.ui.navigation.Navigation
 import your.projectPackage.ui.navigation.Screen
-import your.projectPackage.ui.navigation.composable
 import your.projectPackage.ui.navigation.navigation
 
 @Serializable
@@ -17,6 +17,7 @@ data object ExampleTop : Screen
 fun NavGraphBuilder.examples() {
     navigation<Examples>(startDestination = ExampleTop) {
         composable<ExampleTop> {
+            // ライブラリの composable を使用すべきではない
             ExampleTopScreen()
         }
     }
