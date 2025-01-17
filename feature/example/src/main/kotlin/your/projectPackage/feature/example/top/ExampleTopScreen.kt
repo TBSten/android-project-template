cbf1649c43d0b3c39c940297fb541319d7f6cb4c
TBSten <81161390+TBSten@users.noreply.github.com>
2025-01-18 01:02:30 +0900

    Revert "テスト用に差分を追加"
    This reverts commit a626c004f80870c059848f3221dcbaabce7f28a6.

--- feature/example/src/main/kotlin/your/projectPackage/feature/example/top/ExampleTopScreen.kt
+++ feature/example/src/main/kotlin/your/projectPackage/feature/example/top/ExampleTopScreen.kt
@@ -146,11 +146,4 @@ private fun ExampleTopScreenPreview(
         uiState = uiState,
         dispatch = { },
     )
-    Text("VRT Test Debug: Expect diff")
-}
-
-@Preview
-@Composable
-private fun NewPreview() = PreviewRoot {
-    Text("VRT Test Debug: Expect new")
 }
--- ui/src/main/kotlin/your/projectPackage/ui/component/AppButton.kt
+++ ui/src/main/kotlin/your/projectPackage/ui/component/AppButton.kt
@@ -2,8 +2,11 @@ package your.projectPackage.ui.component
 
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import your.projectPackage.ui.PreviewRoot
 
 @Composable
 fun AppButton(onClick: () -> Unit, modifier: Modifier = Modifier, content: @Composable RowScope.() -> Unit) {
@@ -12,4 +15,12 @@ fun AppButton(onClick: () -> Unit, modifier: Modifier = Modifier, content: @Comp
         content = content,
         modifier = modifier,
     )
+}
+
+@Preview
+@Composable
+private fun AppButtonPreview() = PreviewRoot {
+    AppButton(onClick = {}) {
+        Text("AppButtonPreview")
+    }
 }
