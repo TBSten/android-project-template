064a4945d231fdced4278f864c62b7db4f859e05
TBSten <81161390+TBSten@users.noreply.github.com>
2025-01-25 04:30:28 +0900

    AbstractErrorStateHolder でエラーログを出すように修正

--- domain/error/build.gradle.kts
+++ domain/error/build.gradle.kts
@@ -9,13 +9,3 @@ android {
 
 dependencies {
 }
-
-// androidComponents {
-//    beforeVariants { variantBuilder ->
-//        // To check for a certain build type, use variantBuilder.buildType == "<buildType>"
-//        if (variantBuilder.productFlavors.containsAll(listOf("api" to "minApi21", "mode" to "demo"))) {
-//            // Gradle ignores any variants that satisfy the conditions above.
-//            variantBuilder.enable = false
-//        }
-//    }
-// }
