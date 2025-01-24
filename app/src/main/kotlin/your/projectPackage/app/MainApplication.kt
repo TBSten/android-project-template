6219b9a48ba1e537301a97b7fafb46eab6a3590c
TBSten <81161390+TBSten@users.noreply.github.com>
2025-01-25 04:28:33 +0900

    ログ関数を用意

--- app/src/main/kotlin/your/projectPackage/app/MainApplication.kt
+++ app/src/main/kotlin/your/projectPackage/app/MainApplication.kt
@@ -2,14 +2,12 @@ package your.projectPackage.app
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import timber.log.Timber
+import your.projectPackage.common.initLogger
 
 @HiltAndroidApp
 class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
-        }
+        initLogger()
     }
 }
