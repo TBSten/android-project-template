aee93067aac6b10c8d70dd86c43c2a1396e7a46b
TBSten <81161390+TBSten@users.noreply.github.com>
2025-01-26 01:48:22 +0900

    format

--- debug/src/debug/kotlin/your/projectPackage/debug/ui/DebugMenu.kt
+++ debug/src/debug/kotlin/your/projectPackage/debug/ui/DebugMenu.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
